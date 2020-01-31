54252091b7bdb9492476088d578f2cdaef642b8c
Paweł Psztyć <jarrodek@gmail.com>
2020-01-30 23:43:23 -0800

    first commit

---
+++ build/sw.js
@@ -0,0 +1,71 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "apic-import-b2dc6124.js",
+    "revision": "278f601923e79acbe0105a93b329c372"
+  },
+  {
+    "url": "index.html",
+    "revision": "004e107639fe5309dd11f5b08c302d69"
+  },
+  {
+    "url": "legacy/apic-import-7f7ef545.js",
+    "revision": "9ca8d5d1af540bc1764e3d1815c7d093"
+  },
+  {
+    "url": "polyfills/core-js.577a5602a7262d6256830802d4aaab43.js",
+    "revision": "ccf205728fe514f8276191669b5ea48d"
+  },
+  {
+    "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",
+    "revision": "cff507bc95ad1d6bf1a415cc9c8852b0"
+  },
+  {
+    "url": "polyfills/dynamic-import.b745cfc9384367cc18b42bbef2bbdcd9.js",
+    "revision": "ed55766050be285197b8f511eacedb62"
+  },
+  {
+    "url": "polyfills/fetch.191258a74d74243758f52065f3d0962a.js",
+    "revision": "fcdc4efda1fe1b52f814e36273ff745d"
+  },
+  {
+    "url": "polyfills/regenerator-runtime.92d44da139046113cb3739b173605787.js",
+    "revision": "3aa324bcf8f59cd0eebf46796948aafa"
+  },
+  {
+    "url": "polyfills/systemjs.6dfbfd8f2c3e558918ed74d133a6757a.js",
+    "revision": "683aabfb9b006607885b83e45e9a1768"
+  },
+  {
+    "url": "polyfills/webcomponents.d406f4685fdfb412c61f23b3ae18f2dc.js",
+    "revision": "b1db7cb76380495a55ff4f65a9648f0e"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
